40403b0f7c1f8ef80beb9aebae59daa80121fa49
Jeroen Claassens <jeroen.claassens@live.nl>
2018-05-19 03:48:39 +0200

    fix guild ArgumentType

--- src/types/guild.js
+++ src/types/guild.js
@@ -1,34 +1,42 @@
 const ArgumentType = require('./base');
-const disambiguation = require('../util').disambiguation;
-const escapeMarkdown = require('discord.js').escapeMarkdown;
+const { disambiguation } = require('../util');
+const { escapeMarkdown } = require('discord.js');
 
 class GuildArgumentType extends ArgumentType {
 	constructor(client) {
 		super(client, 'guild');
 	}
 
-	validate(value, msg) {
-		const matches = value.match(/^(?:<#)?([0-9]+)>?$/);
+	validate(val, msg, arg) {
+		const matches = val.match(/^(?:<#)?([0-9]+)>?$/);
 		if(matches) return msg.client.guilds.has(matches[1]);
-		const search = value.toLowerCase();
-		let guilds = msg.client.guilds.filterArray(nameFilterInexact(search));
-		if(guilds.length === 0) return false;
-		if(guilds.length === 1) return true;
+		const search = val.toLowerCase();
+		let guilds = msg.client.guilds.filter(nameFilterInexact(search));
+		if(guilds.size === 0) return false;
+		if(guilds.size === 1) {
+			if(arg.oneOf && !arg.oneOf.includes(guilds.first().id)) return false;
+			return true;
+		}
 		const exactGuilds = guilds.filter(nameFilterExact(search));
-		if(exactGuilds.length === 1) return true;
+		if(exactGuilds.length === 1) {
+			if(arg.oneOf && !arg.oneOf.includes(exactGuilds.first().id)) return false;
+			return true;
+		}
 		if(exactGuilds.length > 0) guilds = exactGuilds;
-		return `${disambiguation(guilds.map(gld => escapeMarkdown(gld.name)), 'guilds', null)}\n`;
+		return guilds.size <= 15 ?
+		`${disambiguation(guilds.map(guild => escapeMarkdown(guild.name)), 'guilds', null)}\n` :
+		'Multiple guilds found. Please be more specific.';
 	}
 
-	parse(value, msg) {
-		const matches = value.match(/^(?:<#)?([0-9]+)>?$/);
+	parse(val, msg) {
+		const matches = val.match(/^(?:<#)?([0-9]+)>?$/);
 		if(matches) return msg.client.guilds.get(matches[1]) || null;
-		const search = value.toLowerCase();
-		const guilds = msg.client.guilds.filterArray(nameFilterInexact(search));
-		if(guilds.length === 0) return null;
-		if(guilds.length === 1) return guilds[0];
+		const search = val.toLowerCase();
+		const guilds = msg.client.guilds.filter(nameFilterInexact(search));
+		if(guilds.size === 0) return null;
+		if(guilds.size === 1) return guilds.first();
 		const exactGuilds = guilds.filter(nameFilterExact(search));
-		if(exactGuilds.length === 1) return exactGuilds[0];
+		if(exactGuilds.length === 1) return exactGuilds.first();
 		return null;
 	}
 }
